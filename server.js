a082005e975f49c2ddfeaab8ead57e1184f21dbe
Cyd Villavicencio <villacv@gmail.com>
2021-12-08 00:25:34 -0500

    database connection added

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image')
 const db = knex({
     client: 'pg',
     connection: {
-        host : 'postgresql-objective-63842',
-        user : '',
-        password : '',
-        database : 'smart-brain'
+        host : process.env.DATABASE_URL,
+        ssl: true,
     }
 });
 
