dfd9a9816995cf7e41e838585cb39039d7c4fc30
Cyd Villavicencio <villacv@gmail.com>
2021-12-08 04:08:02 -0500

    step 1

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # smart-brain-api
-
-The endpoints for my smart-brain project are located here!
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = knex({
     client: 'pg',
     connection: {
         host : '127.0.0.1',
-        user : 'cvilla',
+        user : '',
         password : '',
         database : 'smart-brain'
     }
@@ -25,7 +25,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {res.send('it is working')})
+app.get('/', (req, res) => {res.send(database.users)})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
@@ -33,6 +33,6 @@ app.put('/image', (req, res) => {image.handleImage(req, res, db)} )
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)} )
 
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.POST}`);
+app.listen(3000, () => {
+  console.log("app is running on port 3000")
 })
