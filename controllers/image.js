898311ebc408552badb050e9221ec4e4f97e1924
Cyd Villavicencio <villacv@gmail.com>
2022-07-21 20:19:47 -0400

    updoots

--- controllers/image.js
+++ controllers/image.js
@@ -23,10 +23,10 @@ const handleImage = (req, res, db) => {
     .then(entries => {
       res.json(entries[0].entries);
     })
-    .catch(err => res.status(400).json("unable to get entries"));
-};
+    .catch(err => res.status(400).json("unable to get entries"))
+}
 
 module.exports = {
   handleImage,
   handleApiCall
-};
+}
