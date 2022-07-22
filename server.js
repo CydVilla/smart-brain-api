78377df82c0f1809c59dd13bd9b93dced7fbe8ee
Cyd Villavicencio <villacv@gmail.com>
2022-07-21 22:43:01 -0400

    modified server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex({
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: true,
-  },
+  }
 });
 
 const app = express();
@@ -29,6 +29,6 @@ app.get("/profile/:id", (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put("/image", (req, res) => { image.handleImage(req, res, db)})
 app.post("/imageurl", (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT, () => {
   console.log(`app is running on port ${process.env.PORT}`);
 });
