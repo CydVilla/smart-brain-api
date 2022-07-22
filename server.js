2caac469e5cf4cd3a4f7b5a476e3beb9836e6073
Cyd Villavicencio <villacv@gmail.com>
2022-07-22 10:26:02 -0400

    reconfigured db

--- server.js
+++ server.js
@@ -11,12 +11,10 @@ const image = require("./controllers/image");
 
 const db = knex({
   client: "pg",
-  connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthroized: false,
     }
-  }
 });
 
 const app = express();
